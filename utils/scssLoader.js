792a62cd41c1dc7fa966c95be122962b3a99e3f6
卢逸平 <372254032@qq.com>
2019-09-05 11:41:03 +0800

    set scss-loader options

--- utils/scssLoader.js
+++ utils/scssLoader.js
@@ -6,6 +6,8 @@ const scssLoader = webpackConfig => {
     cmlFile.rule.use = cmlFile.rule.use.map(use => {
       use.options.loaders.scss = JSON.parse(JSON.stringify(use.options.loaders.less)).map(item => {
         if (item.loader === 'less-loader') item.loader = 'sass-loader'
+        if(webpackConfig.resolve.alias['~@output'])item.options.data = '@import "~@output";'
+        else console.warn('请添加 ~@output 别名指向 _output.scss 文件')
         return item
       })
       return use
